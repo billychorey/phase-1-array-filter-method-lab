92b8d1898d75fbe00247147dbb9a7f835ce2b01c
Billy <billychorey@gmail.com>
2023-07-28 17:09:35 -0400

    filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(drivers, name) {
+  return drivers.filter(function (driver) {
+    return driver.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, name) {
+  //return all drivers whose names begin with provided letters - use substring(0)
+  return drivers.filter(function (driver) {
+    return (
+      driver.toLowerCase().substring(0, name.length) === name.toLowerCase()
+    );
+  });
+}
+
+function matchName(drivers, argument) {
+  return drivers.filter(function (driver) {
+    return driver.name === argument;
+  });
+}
